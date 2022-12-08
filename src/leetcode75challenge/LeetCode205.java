87471efd6c102f125b0fd6a99bc3716760026410
Aniket Tiwary <aniket.tiwary111@gmail.com>
2022-12-08 22:33:01 +0530

    LeetCode 205 solution added

--- src/leetcode75challenge/LeetCode205.java
+++ src/leetcode75challenge/LeetCode205.java
@@ -1,2 +1,29 @@
-package leetcode75challenge;public class LeetCode205 {
+package leetcode75challenge;
+
+import java.util.HashMap;
+
+public class LeetCode205 {
+
+    public static boolean isIsomorphic(String s,String t){
+        HashMap<Character,Character> sMap = new HashMap<>();
+        HashMap<Character,Integer> tMap = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(sMap.containsKey(s.charAt(i))) {
+                if (t.charAt(i) != sMap.get(s.charAt(i)))
+                    return false;
+            }else{
+                if(tMap.containsKey(t.charAt(i)))
+                    return false;
+                sMap.put(s.charAt(i),t.charAt(i));
+                tMap.put(t.charAt(i),1);
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        String s = "gadc";
+        String t = "baba";
+        System.out.println("Are strings " + s + " " + t + " Isomorphic : " + isIsomorphic(s,t));
+    }
 }
