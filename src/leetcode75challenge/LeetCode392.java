95a8ea14413a4be89f797b41a0adee34c89d9813
Aniket Tiwary <aniket.tiwary111@gmail.com>
2022-12-08 18:28:57 +0530

    LeetCode Problem 392 solution added

---
+++ src/leetcode75challenge/LeetCode392.java
@@ -0,0 +1,27 @@
+package leetcode75challenge;
+
+public class LeetCode392 {
+
+    private static boolean isIsomorphic(String s, String t) {
+        if(s.equals(""))
+            return true;
+        if(s.length() > t.length())
+            return false;
+        int j = 0;
+        for(int i=0;i<t.length();i++){
+            if(j >= s.length())
+                return true;
+            if(t.charAt(i) == (s.charAt(j)))
+                j++;
+        }
+        return j == s.length();
+    }
+
+
+    public static void main(String args[]){
+        String s = "abc";
+        String t = "axbvfgc";
+        System.out.println("Are the strings Isomorphic : " + isIsomorphic(s,t));
+    }
+
+}
